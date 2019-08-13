ffff1e8e3079f541f92d2d1190bc175a049973d0
yunluo-xingchen <yunluo-xingchen@users.noreply.github.com>
2019-08-13 11:13:38 +0800

    谢涛
    更新了个人中心/充值积分页面

--- ksgl/src/components/personal/Integral.vue
+++ ksgl/src/components/personal/Integral.vue
@@ -4,23 +4,32 @@
       <el-col :span="24" :offset="2">
         <el-tabs value="first">
           <el-tab-pane label="积分充值" name="first">
+            <el-row>
+              <el-col :span="15">
+                <el-table :data="orders" height="250" style="width: 100%">
+                  <el-table-column prop="id" label="订单号"></el-table-column>
+                  <el-table-column prop="rechargeAmount" label="充值金额"></el-table-column>
+                  <el-table-column prop="time" label="下单时间"></el-table-column>
+                  <el-table-column prop="status" label="订单状态"></el-table-column>
+                  <el-table-column prop="address" label="操作">
+                    <template slot-scope="orders">
+                      <el-button
+                        size="mini"
+                        class="el-icon-circle-close"
+                        type="danger"
+                        @click="remove(orders.row.id)"
+                      ></el-button>
+                    </template>
+                  </el-table-column>
+                </el-table>
+              </el-col>
 
-            <el-table :data="orders" height="250" style="width: 70%">
-              <el-table-column prop="id" label="订单号">
-              </el-table-column>
-              <el-table-column prop="rechargeAmount" label="充值金额">
-              </el-table-column>
-              <el-table-column prop="time" label="下单时间">
-              </el-table-column>
-              <el-table-column prop="status" label="订单状态">
-              </el-table-column>
-              <el-table-column prop="address" label="操作">
-                <i class="el-icon-s-grid">&nbsp;</i>
-                <i class="el-icon-shopping-cart-2">&nbsp;</i>
-                <i class="el-icon-error" @click="remove('id')">&nbsp;</i>
-              </el-table-column>
-            </el-table>
-
+              <el-col :span="8" :offset="1">
+                <el-tabs value="first">
+                  <el-tab-pane label="积分充值" name="first"></el-tab-pane>
+                </el-tabs>
+              </el-col>
+            </el-row>
           </el-tab-pane>
         </el-tabs>
       </el-col>
@@ -32,23 +41,26 @@
 export default {
   data() {
     return {
-      orders: [{
-        id: '20190812001',
-        rechargeAmount: '1',
-        time: '2016-05-03',
-        status: '待付款'
-      }, {
-        id: '20190812002',
-        rechargeAmount: '1',
-        time: '2016-05-03',
-        status: '待付款'
-      }]
+      orders: [
+        {
+          id: "20190812001",
+          rechargeAmount: "1",
+          time: "2016-05-03",
+          status: "待付款"
+        },
+        {
+          id: "20190812002",
+          rechargeAmount: "1",
+          time: "2016-05-03",
+          status: "待付款"
+        }
+      ]
     };
   },
   components: {},
   methods: {
-    remove(id){
-      console.log(id)
+    remove(id) {
+      console.log(id);
     }
   }
 };
