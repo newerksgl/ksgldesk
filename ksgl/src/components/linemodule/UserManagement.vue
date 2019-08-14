798f777b93493da64d9d4d50a5365d2996e686b2
yunluo-xingchen <yunluo-xingchen@users.noreply.github.com>
2019-08-14 11:44:37 +0800

    谢涛
    更新了用户模块

--- ksgl/src/components/linemodule/UserManagement.vue
+++ ksgl/src/components/linemodule/UserManagement.vue
@@ -1,19 +1,20 @@
 <template>
   <div>
     <el-row>
-      <el-col :span="6" :offset="6">
+      <el-col :span="24" :offset="1">
         <el-breadcrumb separator-class="el-icon-arrow-right">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>模块管理</el-breadcrumb-item>
+          <el-breadcrumb-item>用户管理</el-breadcrumb-item>
         </el-breadcrumb>
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span="20" :offset="2">
+      <el-col :span="19" :offset="1">
         <el-tabs v-model="activeName">
-          <el-tab-pane label="模块管理" name="first"></el-tab-pane>
+          <el-tab-pane label="用户列表" name="first"></el-tab-pane>
         </el-tabs>
       </el-col>
+      <el-col :span="3" :offset="1"></el-col>
     </el-row>
   </div>
 </template>
