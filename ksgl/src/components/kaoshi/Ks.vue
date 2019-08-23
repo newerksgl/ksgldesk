88b982ad5f85718ae4ebf00fcd97cf90de368b1f
wldxz <54014107+wldxz@users.noreply.github.com>
2019-08-23 11:49:54 +0800

    Update Ks.vue

--- ksgl/src/components/kaoshi/Ks.vue
+++ ksgl/src/components/kaoshi/Ks.vue
@@ -1,5 +1,16 @@
 <template>
-    <div></div>
+    <div>
+        <el-row>
+            <el-col :span="23" :offset="1">
+                <el-breadcrumb separator="/">
+                     <el-breadcrumb-item :to="{ path: '/#/' }">首页</el-breadcrumb-item>
+                    <el-breadcrumb-item><a href="/#/kaoshi">考试管理</a></el-breadcrumb-item>
+                    <el-breadcrumb-item>考试</el-breadcrumb-item>
+                </el-breadcrumb>
+            </el-col>
+        </el-row>
+        <el-row><el-col>&nbsp;</el-col></el-row>
+    </div>
 </template>
 
 <script>
